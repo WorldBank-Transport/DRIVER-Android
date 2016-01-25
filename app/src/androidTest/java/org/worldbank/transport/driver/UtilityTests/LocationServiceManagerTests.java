43d0238529e2813ae15a128025c7e4eb33e382f3
Kathryn Killebrew <kkillebrew@azavea.com>
2016-01-25 17:23:42 -0500

    Test location service manager

--- app/src/androidTest/java/org/worldbank/transport/driver/UtilityTests/LocationServiceManagerTests.java
+++ app/src/androidTest/java/org/worldbank/transport/driver/UtilityTests/LocationServiceManagerTests.java
@@ -1,8 +1,10 @@
 package org.worldbank.transport.driver.UtilityTests;
 
+import android.content.Context;
 import android.test.AndroidTestCase;
 
 import org.worldbank.transport.driver.activities.RecordFormConstantsActivity;
+import org.worldbank.transport.driver.staticmodels.DriverApp;
 import org.worldbank.transport.driver.utilities.LocationServiceManager;
 
 /**
@@ -11,31 +13,37 @@ import org.worldbank.transport.driver.utilities.LocationServiceManager;
 public class LocationServiceManagerTests extends AndroidTestCase {
 
     LocationServiceManager manager;
-    RecordFormConstantsActivity dummyActivity;
+    RecordFormConstantsActivity activity;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         manager = LocationServiceManager.getInstance();
-        dummyActivity = new RecordFormConstantsActivity();
+        activity = new RecordFormConstantsActivity();
     }
 
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
         LocationServiceManager.stopService();
-        dummyActivity.finish();
+        activity.finish();
     }
 
     public void testStatuses() {
         assertEquals("location service should not report running yet",
                  LocationServiceManager.Status.OFF, LocationServiceManager.getCurrentStatus());
 
-        manager.startService(dummyActivity);
+        manager.startService(activity);
 
         assertEquals("location service should have started",
                 LocationServiceManager.Status.AWAITING_GPS, LocationServiceManager.getCurrentStatus());
 
+        LocationServiceManager.stopService();
+
+        assertEquals("location service should have stopped", false, LocationServiceManager.isRunning());
+
+        assertEquals("location service status should show it has stopped",
+                LocationServiceManager.Status.OFF, LocationServiceManager.getCurrentStatus());
 
     }
 }
