d934ecfc16c892c10f0c35fc65b5010e0a7e3074
Kathryn Killebrew <kkillebrew@azavea.com>
2016-03-25 17:33:35 -0400

    Add Google OpenID authorization

--- app/src/main/java/org/worldbank/transport/driver/activities/LoginActivity.java
+++ app/src/main/java/org/worldbank/transport/driver/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package org.worldbank.transport.driver.activities;
 
+import android.accounts.AccountManager;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
@@ -17,6 +18,17 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 
 import org.worldbank.transport.driver.R;
 import org.worldbank.transport.driver.staticmodels.DriverApp;
@@ -29,7 +41,13 @@ import org.worldbank.transport.driver.utilities.LoginUrlBuilder;
 /**
  * A login screen that offers login via username/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoginTask.LoginCallbackListener {
+public class LoginActivity extends AppCompatActivity implements LoginTask.LoginCallbackListener,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String LOG_LABEL = "Login";
+
+    private static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
+    private static final int RC_SIGN_IN = 9001;
 
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -46,6 +64,7 @@ public class LoginActivity extends AppCompatActivity implements LoginTask.LoginC
     private TextView mErrorMessage;
 
     DriverApp app;
+    GoogleApiClient googleApiClient;
 
     // public so server interactions can be mocked in testing
     public LoginTask.LoginUrls mLoginUrlBuilder;
@@ -75,9 +94,6 @@ public class LoginActivity extends AppCompatActivity implements LoginTask.LoginC
         app = mAppContext.getDriverApp();
         mLoginUrlBuilder = new LoginUrlBuilder();
 
-        // TODO: start from a different activity to do this check and bypass loading this activity?
-        // check to see if previous login saved, and skip this screen if so
-
         if (haveSavedUserInfo()) {
             Log.d("LoginActivity", "Have saved user info; skipping login screen");
             Intent intent = new Intent(this, RecordListActivity.class);
@@ -99,6 +115,11 @@ public class LoginActivity extends AppCompatActivity implements LoginTask.LoginC
             }
         });
 
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+        mErrorMessage = (TextView) findViewById(R.id.error_message);
+        mErrorMessage.setText("");
+
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -107,10 +128,34 @@ public class LoginActivity extends AppCompatActivity implements LoginTask.LoginC
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
-        mErrorMessage = (TextView) findViewById(R.id.error_message);
-        mErrorMessage.setText("");
+        // Set up SSO client
+        SignInButton ssoSignInButton = (SignInButton) findViewById(R.id.sso_sign_in_button);
+        String clientId = getString(R.string.oauth_client_id);
+        if (clientId == null || clientId.isEmpty()) {
+            Log.e(LOG_LABEL, "No OAuth client ID defined! Be sure to set it in configurables.xml to enable SSO.");
+            ssoSignInButton.setEnabled(false);
+            return;
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                //.requestIdToken(clientId)
+                .requestServerAuthCode(clientId, true)
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        ssoSignInButton.setSize(SignInButton.SIZE_WIDE);
+        ssoSignInButton.setScopes(gso.getScopeArray());
+        ssoSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptSSO();
+            }
+        });
     }
 
     /**
@@ -230,6 +275,91 @@ public class LoginActivity extends AppCompatActivity implements LoginTask.LoginC
         }
     }
 
+    private void attemptSSO() {
+        Log.d(LOG_LABEL, "attempt sso login");
+        //pickUserAccount();
+        initiateSSO();
+    }
+
+    // another SSO auth guide here:
+    // https://developers.google.com/identity/sign-in/android/sign-in#start_the_sign-in_flow
+    private void initiateSSO() {
+        Intent signInItent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInItent, RC_SIGN_IN);
+    }
+
+    // SSO auth guide here:
+    // https://developers.google.com/android/guides/http-auth#invoke_the_account_picker
+    private void pickUserAccount() {
+        String[] accountTypes = new String[]{"com.google"};
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                accountTypes, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(LOG_LABEL, "handleSignInResult: " + result.isSuccess());
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            if (acct != null) {
+                Log.d(LOG_LABEL, "display name: " + acct.getDisplayName());
+                Log.d(LOG_LABEL, "email: " + acct.getEmail());
+                Log.d(LOG_LABEL, "ID: " + acct.getId());
+                Log.d(LOG_LABEL, "ID token: " + acct.getIdToken());
+                Log.d(LOG_LABEL, "Server auth code: " + acct.getServerAuthCode());
+                if (acct.getPhotoUrl() != null) {
+                    Log.d(LOG_LABEL, "Photo URL: " + acct.getPhotoUrl().getPath());
+                }
+                Log.d(LOG_LABEL, "Scopes: " + acct.getGrantedScopes().toString());
+            } else {
+                Log.e(LOG_LABEL, "SSO result acct is null?!");
+            }
+        } else {
+            Log.d(LOG_LABEL, "Oh noes, SSO failed! TODO: handle stuff");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(LOG_LABEL, "Got activity result!");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_PICK_ACCOUNT) {
+            // Receiving a result from the AccountPicker
+            if (resultCode == RESULT_OK) {
+                String email = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                Log.d(LOG_LABEL, "got back email: " + email);
+                // With the account name acquired, go get the auth token
+                // TODO: getUsername();
+            } else if (resultCode == RESULT_CANCELED) {
+                // The account picker dialog closed without selecting an account.
+                // Notify users that they must pick an account to proceed.
+                Log.w(LOG_LABEL, "Hey! You didn't pick anything.");
+                //TODO: Toast.makeText(this, R.string.pick_account, Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                handleSignInResult(result);
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.w(LOG_LABEL, "SSO sign-in attempt got cancellation result.");
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                Log.e(LOG_LABEL, result.toString());
+                Status status = result.getStatus();
+
+                if (status.getStatusMessage() != null) {
+                    Log.e(LOG_LABEL, status.getStatusMessage());
+                }
+
+                Log.e(LOG_LABEL, "SSO sign-in status code: " + Integer.toString(status.getStatusCode()));
+                Log.e(LOG_LABEL, "has resolution? " + status.hasResolution());
+
+                // TODO: if there is a resolution, launch its pending intent? how to handle?
+
+            }
+        }
+    }
+
     @Override
     public void loginCompleted(DriverUserInfo userInfo) {
         mAuthTask = null;
@@ -255,5 +385,12 @@ public class LoginActivity extends AppCompatActivity implements LoginTask.LoginC
     @Override
     public void loginError(String errorMessage) {
         mErrorMessage.setText(errorMessage);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // TODO: handle
+        Log.e(LOG_LABEL, "Google SSO API connection failed!");
+        Log.e(LOG_LABEL, connectionResult.getErrorMessage());
     }
 }
